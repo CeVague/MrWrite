58a96861c862a6aee0edbc0675bde435c494c503
CeVague <cedric.vachaudez@u-psud.fr>
2017-01-30 17:22:23 +0100

    Compatibilitée et allègement
    Fait en sorte que ça compile sous linux et windows sans avoir de
    fonctions doublons

--- main.c
+++ main.c
@@ -4,223 +4,12 @@
 #include <time.h>
 
 
-//#include "conio21\conio.c"
-
 #include "Structure.h"
 
 #include "GestionTouches.h"
 #include "ManipTranche.h"
 
 
-#if defined(__WIN32__)
-
-#include "conio21\conio.c"
-
-void affiche(Lettre* l){
-    l = (*l).suiv;
-    while((*l).suiv != NULL){
-        printf("%c",(*l).c);
-        l = (*l).suiv;
-    }
-}
-
-void afficheLettre(Lettre* l){
-    while((*l).suiv != NULL){
-        if((*l).c != '\n'){
-            printf("%c",(*l).c);
-        }else{
-            printf("#");
-        }
-        l = (*l).suiv;
-    }
-}
-
-void afficheMot(Mot* m){
-    int ID = 0;
-    while((*m).suiv != NULL){
-        int i;
-        for(i=0;i<(*m).taille;i++){
-            printf("%d", ID);
-        }
-        ID += 1;
-        m = (*m).suiv;
-    }
-}
-
-void afficheLigne(Ligne* l){
-    int ID = 0;
-    while((*l).suiv != NULL){
-        int i;
-        for(i=0;i<(*l).taille;i++){
-            printf("%d", ID);
-        }
-        ID += 1;
-        l = (*l).suiv;
-    }
-}
-/*
-void afficheCurseur(Curseur c){
-    printf("On est en Le/Mo/Li : %d/%d/%d\n", c.lettre, c.mot, c.ligne);
-    printf("Les tailles sont Mo/Li : %d/%d\n", *(c.tailleMot), *(c.tailleLigne));
-    printf("Emplacement : %d/%d\n\n\n", c.placeMot, c.placeLigne);
-}
-*/
-
-
-char randomChar(){
-    int temp = rand()%15;
-
-    switch(temp) {
-        case 0 :
-            return 72;
-            break;
-        case 1 :
-            return 80;
-            break;
-        case 2 :
-            return 77;
-            break;
-        case 3 :
-            return 75;
-            break;
-        case 4 :
-            return 13;
-            break;
-        case 5 :
-            return 8;
-            break;
-        case 6 :
-            return 83;
-            break;
-        case 7 :
-            return 8;
-            break;
-        case 8 :
-            return 8;
-            break;
-        case 9 :
-            return 75;
-            break;
-        case 10 :
-            return ' ';
-            break;
-        default :
-            return (48 + rand()%74);
-    }
-}
-
-void ouvrirFichier(char* chemin, Curseur* curseur){
-    FILE* fichier = NULL;
-    int c = 0;
-
-    fichier = fopen(chemin, "r");
-
-    if (fichier != NULL)
-    {
-        c = fgetc(fichier);
-        while (c != EOF)
-        {
-            inserer(curseur, c);
-            c = fgetc(fichier);
-        }
-
-        fclose(fichier);
-    }
-}
-
-int main()
-{
-    srand(time(NULL));
-
-    Ligne* ligne = newChaineLigne();
-    Mot* mot = (*ligne).debut;
-    Lettre* lettre = (*mot).debut;
-
-    Curseur* curseur = initialise(lettre, mot, ligne, 1, 1);
-
-    printf("Bienvenue dans l'editeur MrWrite\n\n\n");
-    printf("Appuyez sur '²'(cube) pour quiter.\n\nAppyez sur 'o' pour ouvrir un fichier ou 'entree' pour continuer.");
-
-    char c = -3;
-
-    c = getch();
-
-    if(c==-3){
-        return 0;
-    }else if(c==111 || c==79){
-        clrscr();
-        printf("Bienvenue dans l'editeur MrWrite\n\n\n");
-        printf("Entrez le chemin vers le fichier :\n");
-        char temp[100];
-        scanf("%s", temp);
-        ouvrirFichier(temp, curseur);
-    }
-
-    clrscr();
-
-    do{
-        printf("Bienvenue dans l'editeur MrWrite\n\n\n");
-
-        inserer(curseur, '|');
-        affiche(lettre);
-        toucheDel(curseur);
-/*
-        printf("\n\n\n\n");
-        printf("%d", c);
-        printf("\n\n\n\n");
-        afficheLettre(lettre);
-        printf("\n");
-        afficheMot(mot);
-        printf("\n");
-        afficheLigne(ligne);
-        printf("\n");
-        printf("(%d, %d)", (*curseur).placeMot, (*curseur).placeLigne);
-        printf("\n");
-*/
-        c = getch();
-        //c = randomChar();
-
-        clrscr();
-
-        switch(c) {
-            case -3 :
-                break;
-            case -32 :
-                break;
-            case 72 :
-                haut(curseur);
-                break;
-            case 80 :
-                bas(curseur);
-                break;
-            case 77 :
-                droite(curseur);
-                break;
-            case 75 :
-                gauche(curseur);
-                break;
-            case 13 :
-                inserer(curseur, '\n');
-                break;
-            case 8 :
-                toucheDel(curseur);
-                break;
-            case 83 :
-                toucheSuppr(curseur);
-                break;
-            default :
-                inserer(curseur, c);
-        }
-
-    }while(c != -3);
-
-
-    return 0;
-}
-
-#endif
-
-#if defined(__LINUX__)
 void affiche(Lettre* l){
     l = (*l).suiv;
     while((*l).suiv != NULL){
@@ -277,7 +66,6 @@ void afficheCurseur(Curseur c){
 }
 */
 
-
 char randomChar(){
     int temp = rand()%15;
 
@@ -339,6 +127,93 @@ void ouvrirFichier(char* chemin, Curseur* curseur){
     }
 }
 
+
+
+#ifdef _WIN32
+
+#include "conio21\conio.c"
+
+int main()
+{
+    srand(time(NULL));
+
+    Ligne* ligne = newChaineLigne();
+    Mot* mot = (*ligne).debut;
+    Lettre* lettre = (*mot).debut;
+
+    Curseur* curseur = initialise(lettre, mot, ligne, 1, 1);
+
+    printf("Bienvenue dans l'editeur MrWrite\n\n\n");
+    printf("Appuyez sur '²'(cube) pour quiter.\n\nAppyez sur 'o' pour ouvrir un fichier ou 'entree' pour continuer.");
+
+    char c = -3;
+
+    c = getch();
+
+    if(c==-3){
+        return 0;
+    }else if(c==111 || c==79){
+        clrscr();
+        printf("Bienvenue dans l'editeur MrWrite\n\n\n");
+        printf("Entrez le chemin vers le fichier :\n");
+        char temp[100];
+        scanf("%s", temp);
+        ouvrirFichier(temp, curseur);
+    }
+
+    clrscr();
+
+    do{
+        printf("Bienvenue dans l'editeur MrWrite\n\n\n");
+
+        inserer(curseur, '|');
+        affiche(lettre);
+        toucheDel(curseur);
+        c = getch();
+        //c = randomChar();
+
+        clrscr();
+
+        switch(c) {
+            case -3 :
+                break;
+            case -32 :
+                break;
+            case 72 :
+                haut(curseur);
+                break;
+            case 80 :
+                bas(curseur);
+                break;
+            case 77 :
+                droite(curseur);
+                break;
+            case 75 :
+                gauche(curseur);
+                break;
+            case 13 :
+                inserer(curseur, '\n');
+                break;
+            case 8 :
+                toucheDel(curseur);
+                break;
+            case 83 :
+                toucheSuppr(curseur);
+                break;
+            default :
+                inserer(curseur, c);
+        }
+
+    }while(c != -3);
+
+
+    return 0;
+}
+
+#endif
+
+#ifdef linux
+
 char getch(){
     system("/bin/stty raw");
     char c = getchar();
@@ -347,7 +222,8 @@ char getch(){
     return c;
 }
 
-void boucleBest(){
+int main()
+{
     srand(time(NULL));
 
     Ligne* ligne = newChaineLigne();
@@ -454,21 +330,7 @@ void boucleBest(){
         }
 
     }while(c != -109);
-}
-
-int main()
-{
-    boucleBest();
-
-    /*
-    char c;
-    while(1==1){
-        c = getch();
-        printf("\n%d\n", c);
-
-    }
-    */
 
     return 0;
 }
-#endif 
+#endif
